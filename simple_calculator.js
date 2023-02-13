573c68a7ddbf2ac940e2e23aa0aa4ce5ddf063e5
van-eus <120988816+van-eus@users.noreply.github.com>
2023-02-13 17:46:24 +0200

    Update simple_calculator.js

--- simple_calculator.js
+++ simple_calculator.js
@@ -1,4 +1,4 @@
-//the simple calculator using nodejs and express
+//The SIMPLE CALCULATOR FOR ADDITION AND MULTIPLICATION
 const express = require('express');
 const app = express();
 const port = 7600;
@@ -7,25 +7,28 @@ const port = 7600;
 //this'll define a new endpoint that accepts an array of numbers and returns the sum of those numbers
 
 app.get('/add', (req, res) => {
-  const numb = req.query.numb;
-  console.log(numb);
+  let numb = req.query.numb;
+  let numbers = numb.split(',');
   let add = 0;
-  for (let n = 0; n < numb.length; n++) {
-    add += parseInt(numb[n]);
+  for (let n = 0; n < numbers.length; n++) {
+    add += parseInt(numbers[n]);
   }
   res.send({ add });
 });
 
+
+
 //this RESTful APIs will define a new endpoint that accepts an array of numbers and returns the product of those numbers
 app.get('/product', (req, res) => {
-  const numb = req.query.numb;
-  console.log(numb);
+  let numb = req.query.numb;
+  let numbers = numb.split(',');
   let product = 1;
-  for (let n = 0; n < numb.length; n++) {
-    product *= parseInt(numb[n]);
+  for (let n = 0; n < numbers.length; n++) {
+    product *= parseInt(numbers[n]);
   }
   res.send({ product });
 });
+
 
 app.listen(port, () => {
     console.log(`The simple calculator server is running on port ${port}....`)
